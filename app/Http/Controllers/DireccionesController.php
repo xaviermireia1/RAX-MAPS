262f7e0b1cb80b04181a49e23f58afb9784d18ea
Raul <100007530.joan23@fje.edu>
2022-03-03 19:47:21 +0100

    CRUD Etiquetas + Sessions + Fitxer Validacion Loggin

--- app/Http/Controllers/DireccionesController.php
+++ app/Http/Controllers/DireccionesController.php
@@ -3,9 +3,61 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Exception;
+use Illuminate\Support\Facades\Storage;
+use PhpParser\Node\Stmt\TryCatch;
+use App\Http\Requests\LoginValidacion;
 
 class DireccionesController extends Controller
 {
+    //Mostrar
+    public function mostrarEtiqueta(){
+        $listaEtiquetas = DB::table('tbl_etiqueta')->select('*')->get();
+        return view('mostrar', compact('listaEtiquetas'));
+    }
+
+    //Crear
+    public function crearEtiquetasPost(Request $request){
+        $datos = $request->except('_token');
+        try{
+            DB::beginTransaction();
+            DB::table('tbl_etiqueta')->insertGetId(['nombre_eti'=>$datos['nombre_eti'],'icono_eti'=>$datos['icono_eti'],'id_usuario'=>$datos['id_usuario']]);
+            DB::commit();
+        }catch(\Exception $e){
+            DB::rollBack();
+            return $e->getMessage();
+        }
+        return redirect('');
+    }
+
+    //Modificar
+    public function modificarEtiquetaPut(Request $request){
+        $datos = $request->except('_token','_method');
+        try{
+            DB::beginTransaction();
+            DB::table('tbl_etiqueta')->where('id','=',$datos['id'])->update($datos);
+            DB::commit();
+        }catch(\Exception $e){
+            DB::rollBack();
+            return $e->getMessage();
+        }
+        return redirect('');
+    }
+
+    //Eliminar
+    public function eliminarEtiquetas($id){
+        try{
+            DB::beginTransaction();
+            DB::table('tbl_etiqueta')->where('id','=',$id)->delete();
+            DB::commit();
+        }catch(\Exception $e){
+            DB::rollBack();
+            return $e->getMessage();
+        }
+        return redirect('');
+    }
+
     /**
      * Display a listing of the resource.
      *
