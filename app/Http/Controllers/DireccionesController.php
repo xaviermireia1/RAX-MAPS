a12426dc937bc16d95561963c48fa282bfaa3622
Xavi <100006207.joan23@fje.edu>
2022-03-11 17:20:27 +0100

    Modificar direccion completado

--- app/Http/Controllers/DireccionesController.php
+++ app/Http/Controllers/DireccionesController.php
@@ -129,19 +129,45 @@ class DireccionesController extends Controller
         $direccionModal = DB::select("SELECT * FROM tbl_ubicacion where id=$id");
         return response()->json($direccionModal);
     }
+
+
     public function modificarDireccionPut(Request $request){
-        $datos=$request->except('_token');
+        $datos = $request->except('_token','_method');
+        $foto = DB::table('tbl_ubicacion')->select('foto_ubi')->where('id','=',$datos['id_ubicacion'])->first();  
+        if($request->hasFile('foto_ubi')){ 
+            if ($foto->foto_ubi != null) {
+                if (file_exists('storage/'.$foto->foto_ubi)) {
+                    Storage::delete('public/'.$foto->foto_ubi);
+                }
+            }
+            $datos['foto_ubi'] = $request->file('foto_ubi')->store('uploads','public');
+        }else{
+            $datos['foto_ubi'] = $foto->foto_ubi;
+        }
+        $idEtiqueta = DB::select("SELECT regi.* FROM tbl_registro regi
+        INNER JOIN tbl_etiqueta eti ON regi.id_etiqueta = eti.id
+        WHERE regi.id_ubicacion = {$datos['id_ubicacion']} AND eti.id_usuario = 1");
         try {
             DB::beginTransaction();
-            DB::table('tbl_ubicacion')->where('id','=',$datos['id'])->update($datos);
+            DB::select("UPDATE tbl_registro
+            INNER JOIN tbl_etiqueta ON tbl_registro.id_etiqueta = tbl_etiqueta.id
+            SET tbl_registro.id_etiqueta = {$datos['id_eti']}
+            WHERE tbl_registro.id_ubicacion = {$datos['id_ubicacion']} AND tbl_etiqueta.id_usuario = 1");
+            //$datos = $request->except('_token','_method',$datos['id_eti']);
+            //DB::table('tbl_ubicacion')->where('id','=',$datos['id_ubicacion'])->update($datos);
+            DB::select("UPDATE tbl_ubicacion 
+            SET nombre_ubi='{$datos['nombre_ubi']}',descripcion_ubi='{$datos['descripcion_ubi']}',latitud_ubi='{$datos['latitud_ubi']}',longitud_ubi='{$datos['longitud_ubi']}',direccion_ubi='{$datos['direccion_ubi']}',foto_ubi='{$datos['foto_ubi']}'
+            WHERE id = {$datos['id_ubicacion']}");
             DB::commit();
+            return response()->json(array('resultado'=> 'OK'));
         } catch (\Exception $e) {
             DB::rollBack();
-            return $e->getMessage();
+            return response()->json(array('resultado'=> 'NOK: '.$e->getMessage()));
         }
-        return redirect('');
     }
 
+    
+    
     public function modificarEtiquetaPut(Request $request){
         $datos = $request->except('_token','_method');
         try{
